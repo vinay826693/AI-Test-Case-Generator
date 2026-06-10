759d38aca762f46daeabf03c34a153a3c314c1f3
Manpreet Bains <43449726+bainscoder@users.noreply.github.com>
2026-06-10 15:58:54 +0530

    Update

--- automation/security/zap-scan.sh
+++ automation/security/zap-scan.sh
@@ -2,10 +2,20 @@
 
 mkdir -p security/reports
 
+if [ "$GITHUB_ACTIONS" = "true" ]; then
+  TARGET="http://localhost:5173"
+  NETWORK="--network host"
+else
+  TARGET="http://host.docker.internal:5173"
+  NETWORK=""
+fi
+
 docker run --rm \
+  --user root \
+  $NETWORK \
   -v "$(pwd)/security/reports:/zap/wrk" \
   ghcr.io/zaproxy/zaproxy:stable \
   zap-baseline.py \
-  -t http://host.docker.internal:5173 \
+  -t "$TARGET" \
   -r zap-report.html \
   -J zap-report.json
